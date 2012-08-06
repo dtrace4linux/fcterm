06c327aab792b792be7e8b9f0951bd863ff902f2
Paul D Fox <paul.d.fox@gmail.com>
2012-08-06 12:18:46 +0100

    153* ctw.c: Allow KP_0 to be an INS key innon-keypad mode.

--- include/build.h
+++ include/build.h
@@ -1,6 +1,6 @@
 # define	MAJ_VERSION	4
 # define	MIN_VERSION	32
-int	version_build_no = 152;
-# define VERSION_BUILD_NO 152
-char	version_build_date[] = "Sun Jul  8 18:23:15 2012";
+int	version_build_no = 153;
+# define VERSION_BUILD_NO 153
+char	version_build_date[] = "Mon Aug  6 12:17:54 2012";
 
