cd17dd907df4609127b562146f43ea19b346eb0a
Paul D Fox <paul.d.fox@gmail.com>
2019-10-24 17:13:22 +0100

    update to 4.039

--- include/build.h
+++ include/build.h
@@ -1,5 +1,5 @@
 # define	MAJ_VERSION	4
-# define	MIN_VERSION	38
+# define	MIN_VERSION	39
 int	version_build_no = 180;
 # define VERSION_BUILD_NO 180
 char	version_build_date[] = "Sat Dec 30 22:05:39 2017";
