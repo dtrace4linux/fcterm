a14d0e14686f4c48728f56fe5dffae13714b147f
Paul D Fox <paul.d.fox@gmail.com>
2019-12-18 22:48:10 +0000

    Release b

--- include/build.h
+++ include/build.h
@@ -2,7 +2,7 @@
 # define	MIN_VERSION	40
 int	version_major = MAJ_VERSION;
 int	version_minor = MIN_VERSION;
-int	version_build_no = 187;
-# define VERSION_BUILD_NO 187
-char	version_build_date[] = "Wed Dec 18 22:48:07 GMT 2019";
+int	version_build_no = 188;
+# define VERSION_BUILD_NO 188
+char	version_build_date[] = "Wed Dec 18 22:48:10 GMT 2019";
 
