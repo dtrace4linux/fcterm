737237d7c0cef380aeffaa0e0aefcf192a713a77
Paul D Fox <paul.d.fox@gmail.com>
2020-04-16 16:14:24 +0100

    Release b

--- include/build.h
+++ include/build.h
@@ -2,8 +2,8 @@
 # define	MIN_VERSION	40
 int	version_major = MAJ_VERSION;
 int	version_minor = MIN_VERSION;
-int	version_build_no = 211;
-# define VERSION_BUILD_NO 211
-char	version_build_date[] = "Wed Apr 15 13:06:11 BST 2020";
+int	version_build_no = 212;
+# define VERSION_BUILD_NO 212
+char	version_build_date[] = "Thu Apr 16 16:14:21 BST 2020";
 int	version_build_date_yyyymmdd = 20200401;
 
