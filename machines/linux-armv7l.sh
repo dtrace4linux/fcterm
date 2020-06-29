07b009673cc08675cbc12aa8c95deef0e6f8f306
Paul D Fox <paul.d.fox@gmail.com>
2020-06-29 11:20:29 +0100

    add new files

---
+++ machines/linux-armv7l.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+CC=${CC:-gcc -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0 -g -W -Wall -fno-stack-protector -fno-inline -fno-strict-aliasing}
+CC="$CC -DHAVE_FREETYPE_XFT `pkgconf --cflags freetype2`"
+CFLAGS="-DCR_LINUX_ARMV7L -DCR_LINUX"
+#FT=-lXft
+OBJDIR=${OBJDIR:-bin.$OS_TYPE}
+XAW_LINK="$FT -lXaw -lXmu -lXext -lXt -lSM -lICE -lX11 -ldl"
+MOTIF_LINK="-lXm -lXt -lSM -lICE -lX11 -ldl"
+
+export CC
+export CFLAGS
+export OBJDIR
+export XAW_LINK
+export MOTIF_LINK
---
+++ tools/packages.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+err=0
+for i in "/usr/bin/pkgconf pkgconf" \
+	"/usr/include/X11/Xaw/Label.h libxaw7-dev" \
+	"/usr/include/X11/Xft/Xft.h libxft-dev"
+do
+	set -- $i
+	fn=$1
+	pkg=$2
+	if [ ! -f $fn ]; then
+		echo "$fn is missing, from package $pkg"
+		echo ""
+		echo "Please run:"
+		echo ""
+		echo "  apt-get install $pkg"
+		echo ""
+		err=1
+	fi
+done
+exit $err
